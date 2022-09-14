caa87a38f959e516ae02756b550314fd28dde3d7
eotel <miura@omororg.com>
2022-09-14 22:24:13 +0900

    fix: workbox-windowをdevDependenciesに追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
 });
